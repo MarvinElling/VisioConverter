cb53c2cd3f12384ef93429dd21a3579fbb8da1b0
Marvin Elling <marvin.elling@rohde-schwarz.com>
2025-11-24 13:09:58 +0100

    linting

--- test/test_converter.py
+++ test/test_converter.py
@@ -1,15 +1,16 @@
-from visioconverter import Converter
+"""Test Converter."""
 
-src_path = "src_dir/example1.vsdx"
+from visioconverter import Converter  # pylint: disable=E0401
+
+src_path = 'test/src_dir/example1.vsdx'
+out_path = 'test/out_dir/'
 
 Converter.vsdx2svg(
     src_path=src_path,
-    out_dir="out_dir",
-    visible=False,
+    out_dir=out_path,
 )
 
 Converter.folder_vsdx2svg(
-    src_folder="src_dir",
-    out_folder="out_dir",
-    visible=False,
+    src_folder='test/src_dir',
+    out_folder=out_path,
 )
